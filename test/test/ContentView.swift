a046e491d8b8f1a4fe533dbb3954e9d556fd9bc2
melindatsay <melinda0619@gmail.com>
2022-06-26 10:12:30 -0700

    Initial Commit

---
+++ test/test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Melinda Tsay on 2022/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Melinda Tsay on 2022/6/26.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
